dfd93bfa1e85852d6100b8b5b269f5c5236eeaec
Advait Arumugam <advaitarumugam@ucsb.edu>
2022-03-29 11:27:27 -0700

    initial commit

---
+++ helloWorld.cpp
@@ -0,0 +1,9 @@
+// helloWorld.cpp  Z.Matni for UCSB CS32 S22 
+// Edited by: Advait Arumugam
+// minimal Hello World! program for testing Makefiles 
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
